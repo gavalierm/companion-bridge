1c73bf77ddc2071c04553a26760ee993f9399ad7
gavalierm <marcel@gavalier.sk>
2023-02-07 16:25:05 +0100

    tmux

--- install.sh
+++ install.sh
@@ -9,3 +9,12 @@ git stash
 git pull
 
 sudo apt-get install -y tmux nodejs vim
+
+sudo service cron status
+sudo systemctl enable cron
+sudo systemctl start cron
+
+cmd="@reboot $DST_PATH/auto_tmuxer.sh >>/tmp/atem_cron.log 2>&1"
+(crontab -u pi -l; echo "$cmd" ) | crontab -u pi -
+cmd="0 1 * * * $DST_PATH/auto_tmuxer.sh >>/tmp/atem_cron.log 2>&1"
+(crontab -u pi -l; echo "$cmd" ) | crontab -u pi -
